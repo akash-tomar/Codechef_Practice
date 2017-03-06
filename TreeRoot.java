49fb75d7513d2d523606983a96e4aeeea03b54d2
Akash Tomar <akash.tomar217@gmail.com>
2017-03-06 21:51:26 +0530

    WIP

---
+++ TreeRoot.java
@@ -0,0 +1,14 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class TreeRoot {
+	public static void main(String[] args) {
+		int t;
+		Scanner s = new Scanner(System.in);
+		t = s.nextInt();
+		for(int i=0;i<t;i++) {
+			
+		}
+	}
+}
