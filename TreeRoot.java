7b2eaefea87c48c5fae21872424d79fae7fb41de
Akash Tomar <akash.tomar217@gmail.com>
2017-03-06 21:56:25 +0530

    treeroot solved

--- TreeRoot.java
+++ TreeRoot.java
@@ -8,7 +8,14 @@ public class TreeRoot {
 		Scanner s = new Scanner(System.in);
 		t = s.nextInt();
 		for(int i=0;i<t;i++) {
-			
+			int n=s.nextInt();
+			int root = 0;
+			for(int j=0;j<n;j++) {
+				int x = s.nextInt();
+				int y = s.nextInt();
+				root += (x-y);
+			}
+			System.out.println(root);
 		}
 	}
 }
